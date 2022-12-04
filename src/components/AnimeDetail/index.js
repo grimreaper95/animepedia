4b72c156173879e3f5b112eceb807a3381a65e8a
nishamishra12 <nishamishra093@gmail.com>
2022-12-04 14:40:33 -0500

    Added streaming link to detail

--- src/components/AnimeDetail/index.js
+++ src/components/AnimeDetail/index.js
@@ -8,6 +8,8 @@ const AnimeDetail = () => {
     const [animeInfo, setAnimeDetail] = useState([]);
     const [animeImage, setAnimeImage] = useState([]);
     const [animeTrailer, setAnimeTrailer] = useState([]);
+    const [animeGenre, setAnimeGenre] = useState([]);
+    const [animeStreaming, setAnimeStream] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -17,6 +19,8 @@ const AnimeDetail = () => {
             setAnimeDetail(animeData.data)
             setAnimeImage(animeData.data.images.jpg)
             setAnimeTrailer(animeData.data.trailer)
+            setAnimeGenre(animeData.data.genres)
+            setAnimeStream(animeData.data.streaming)
             console.log(animeData);
         }
         fetchData()
@@ -40,14 +44,13 @@ const AnimeDetail = () => {
                         </span>
                     </Col>
                 </Row>
-                <Row>
-                    <Col xs={6} md={4} lg={3}>
-                        <img src={animeImage.image_url} />
+
+                <Row className="mt-3 photo-section">
+                    <Col xs={6} md={4} lg={3} >
+                        <img height="320" src={animeImage.image_url} />
                     </Col>
                     <Col className="video-responsive">
                         <iframe
-                            width="853"
-                            height="480"
                             src={animeTrailer.embed_url}
                             frameBorder="0"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -56,22 +59,20 @@ const AnimeDetail = () => {
                         />
                     </Col>
                 </Row>
-                {/* <div class="card-group">
-                    <div class="card">
-                        <img class="card-img-top max-height-img" src={animeImage.image_url} alt="Card image cap"/>
+            
+                <div class="card mt-3">
+                    <div class="card-body">
+                    {animeGenre.map((genre, a) => (
+                            <span type="button" class="badge rounded-pill bg-dark m-2 card-title">{genre.name}</span>
+                        ))}
+                        <p class="card-text">{animeInfo.synopsis}</p>
+                        {animeStreaming.map((stream, a) => (
+                            <a href={stream.url} class="card-link"><img width="20" height="20" src={"../../images/" + stream.name +".png"} /></a>
+                    
+                        ))}
+                        <a href="#" class="card-link">Another link</a>
                     </div>
-                    <div class="card video-responsive">
-                    <iframe
-                            width="853"
-                            height="480"
-                            src={animeTrailer.embed_url}
-                            frameBorder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                            allowFullScreen
-                            title="Embedded youtube"
-                        />
-                    </div>
-                </div> */}
+                </div>
             </Container>
         </>
     )
