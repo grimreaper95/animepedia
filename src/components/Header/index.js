1f5892da3dab27f513745cf5ec1f45d67d160527
kanishkasoni <soni.kan@northeastern.edu>
2022-12-06 15:43:33 -0500

    admin login

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -38,7 +38,8 @@ const HeaderBar = () => {
                     <>
                         <Nav.Link>Welcome {currentUser.username}</Nav.Link>
                         <Nav.Link onClick={handleLogout} className="nav-link" href="#">Logout</Nav.Link>
-                        <Link to="/profile" className="nav-link" href="#">Profile</Link>
+                        <Nav.Link to="/profile" className="nav-link" href="#">Profile</Nav.Link>
+
                     </>
                 }
                 {
@@ -46,6 +47,7 @@ const HeaderBar = () => {
                     <>
                         <Link to="/login" className="nav-link" href="#">Login</Link>
                         <Link to="/register" className="nav-link" href="#">Sign Up</Link>
+                        <Link to="/admin/login" className="nav-link" href="#">Admin</Link>
                     </>
                 }
 
