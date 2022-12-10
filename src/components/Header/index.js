f6b9f2ad9cb0430eb86d44407b3f4c9979321c45
kanishkasoni <soni.kan@northeastern.edu>
2022-12-10 13:50:13 -0500

    header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import {Navbar, Container, Nav, NavDropdown} from "react-bootstrap";
 // import logo_test from "../../images/logo_test.png"
 import './index.css'
 import {useDispatch, useSelector} from "react-redux";
@@ -9,6 +9,7 @@ import {useNavigate} from "react-router";
 
 const HeaderBar = () => {
     const {currentUser} = useSelector(state => state.userData)
+    const {currentAdmin} = useSelector(state => state.adminData)
     const dispatch = useDispatch()
     const navigate = useNavigate()
     const handleLogout = () => {
@@ -16,46 +17,55 @@ const HeaderBar = () => {
         dispatch(logoutThunk())
         navigate('../')
     }
-    useEffect(()=>{
+    useEffect(() => {
         dispatch(profileThunk())
     }, [])
     return (
         <>
-        <Navbar className="shadow-lg mb-5 bg-body rounded">
-            <Container>
-            <Nav className="me-auto">
-                <Navbar.Brand href="/">
-                  <img
-                      src={"../../../images/logo_test.png"}
-                      className="d-inline-block align-top logo-image"
-                      alt="AnimePedia"
-                  />
-                  <span className="logo-text">
+            <Navbar className="shadow-lg mb-5 bg-body rounded">
+                <Container>
+                    <Nav className="me-auto">
+                        <Navbar.Brand href="/">
+                            <img
+                                src={"../../../images/logo_test.png"}
+                                className="d-inline-block align-top logo-image"
+                                alt="AnimePedia"
+                            />
+                            <span className="logo-text">
                       AnimePedia
                   </span>
-                </Navbar.Brand>
-            </Nav>
-            <Nav>
-                {
-                    currentUser &&
-                    <>
-                        <Nav.Link>Welcome {currentUser.username}</Nav.Link>
-                        <Nav.Link onClick={handleLogout} className="nav-link" href="#">Logout</Nav.Link>
-                        <Link to="/profile" className="nav-link" href="#">Profile</Link>
-                    </>
-                }
-                {
-                    !currentUser &&
-                    <>
-                        <Link to="/login" className="nav-link" href="#">Login</Link>
-                        <Link to="/register" className="nav-link" href="#">Sign Up</Link>
-                        <Link to="/admin/login" className="nav-link" href="#">Admin</Link>
-                    </>
-                }
+                        </Navbar.Brand>
+                    </Nav>
+                    <Nav>
+                        {
+                            currentUser &&
+                            <>
+                                <Nav.Link>Welcome {currentUser.username}</Nav.Link>
+                                <Nav.Link onClick={handleLogout} className="nav-link" href="#">Logout</Nav.Link>
+                                <Link to="/profile" className="nav-link" href="#">Profile</Link>
+                            </>
+                        }
+                        {
+                            !currentUser &&
+                            <>
+                                <Link to="/login" className="nav-link" href="#">Login</Link>
+                                <Link to="/register" className="nav-link" href="#">Sign Up</Link>
+                                {
+                                    currentAdmin &&
+                                    <Link to="/admin" className="nav-link" href="#">Admin</Link>
+                                }
+                                {
+                                    !currentAdmin &&
+                                    <Link to="/admin/login" className="nav-link" href="#">Admin</Link>
+                                }
 
-            </Nav>
-            </Container>
-          </Navbar>
+
+                            </>
+                        }
+
+                    </Nav>
+                </Container>
+            </Navbar>
         </>
     );
 }
